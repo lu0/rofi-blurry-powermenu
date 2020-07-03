8e224921d94c29fc1df49c045deb457971316f4d
lu0 <lucero.alvaradorz@gmail.com>
2020-07-03 00:43:02 -0500

    Relative font size for 16:9 resolutions

--- powermenu.sh
+++ powermenu.sh
@@ -7,10 +7,6 @@
 # Github : @lu0
 #
 
-# Blur desktop screenshot to use as a fake background
-SS_PATH="$HOME/.config/rofi/screenshot.png"
-import -window root $SS_PATH && convert $SS_PATH -blur 0x10 -auto-level +depth $SS_PATH
-
 # Options as characters
 # Copied from decoded unicodes.
 # (Private use of "Feather" font)
@@ -22,9 +18,20 @@ lock="";            # "\uE98F"
 
 options="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
 
+# Blur desktop screenshot to use as a fake background
+SS_PATH="$HOME/.config/rofi/screenshot.png"
+import -window root $SS_PATH && convert $SS_PATH -blur 0x10 -auto-level +depth $SS_PATH
+
+# Font size according to screen dimensions
+DEFAULT_FONTSIZE=60
+DEFAULT_WIDTH=1920
+CURRENT_WIDTH=$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3}')
+FONTSIZE=$(echo "$CURRENT_WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
+
 selected="$(echo -e "$options" | 
             rofi -show-icons -theme fullscreen_powermenu.rasi \
                  -fake-background $SS_PATH -fake-transparency \
+                 -font "WeblySleek UI Normal $FONTSIZE" \
                  -p "See you later, ${LOGNAME^}!" -dmenu -selected-row 4)"
 
 case $selected in
