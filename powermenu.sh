a5f8f0d1366d998da86d5ec9ffb763a9f849d2a9
lu0 <lucero.alvaradorz@gmail.com>
2020-07-03 06:58:59 -0500

    use jpg for fast blur

--- powermenu.sh
+++ powermenu.sh
@@ -8,29 +8,29 @@
 #
 
 # Options as characters
-# Copied from decoded unicodes.
-# (Private use of "Feather" font)
+# Copied from decoded unicodes (private use of "Feather" font)
 shutdown="";        # "\uE9C0"
 reboot="";          # "\uE9C4"
 sleep="";           # "\uE9A3"
 logout="";          # "\uE991"
 lock="";            # "\uE98F"
-
 options="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
 
-# Blur desktop screenshot to use as a fake background
-SS_PATH="$HOME/.config/rofi/screenshot.png"
-import -window root $SS_PATH && convert $SS_PATH -blur 0x10 -auto-level +depth $SS_PATH
+# Fake blurred background
+SS_PATH="$HOME/.config/rofi/screenshot"
+rm -f "${SS_PATH}.jpg" && scrot -z "${SS_PATH}.jpg"                 # screenshot
+convert "${SS_PATH}.jpg" -blur 0x10 -auto-level "${SS_PATH}.jpg"    # blur
+convert "${SS_PATH}.jpg" "${SS_PATH}.png"                           # rofi reads png images
 
 # Font size according to screen dimensions
-DEFAULT_FONTSIZE=60
 DEFAULT_WIDTH=1920
-CURRENT_WIDTH=$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3}')
-FONTSIZE=$(echo "$CURRENT_WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
+WIDTH=$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3}')
+DEFAULT_FONTSIZE=60
+FONTSIZE=$(echo "$WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
 
 selected="$(echo -e "$options" | 
             rofi -show-icons -theme fullscreen_powermenu.rasi \
-                 -fake-background $SS_PATH -fake-transparency \
+                 -fake-background ${SS_PATH}.png -fake-transparency \
                  -font "WeblySleek UI Normal $FONTSIZE" \
                  -p "See you later, ${LOGNAME^}!" -dmenu -selected-row 4)"
 
